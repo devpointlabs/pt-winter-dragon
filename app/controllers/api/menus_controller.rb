114ded194ede54988150cb5146155c24915771dd
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2019-01-10 20:59:47 -0700

    set up menu controller

--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,16 +1,44 @@
 class Api::MenusController < ApplicationController
+  
   def index
+    render json: Menu.all
   end
 
   def show
+    render json: @menu
+  end
+
+  def create
+    menu = Menu.new(menu_params)
+
+    if menu.save
+      render json: menu
+    else
+      render json: menu.errors, status: 422
+    end
   end
 
   def edit
   end
 
   def update
+    if @menu.update(menu_params)
+      render json: @menu
+    else
+      render json: menu.errors, status: 422
+    end
   end
 
   def destroy
+    @blog.destroy
   end
+
+  private
+    def menu_params 
+      @menu = Menu.find(params[:id])
+    end
+
+    def set_menu
+      
+    end
 end
--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,1 +1,29 @@
 import React from 'react';
+import MenuForm from './components/MenuForm';
+
+class Menu extends React.Component {
+  state = { menu: [] }
+
+  showMenu = () => {
+    return (
+      <div>Menu</div>
+    )
+  }
+
+  submit = (menu) => {
+    console.log(menu);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.showMenu()}
+        <br />
+        <h2>Add a new menu</h2>
+        <MenuForm submit={this.submit} />
+      </div>
+    )
+  }
+}
+
+export default Menu;
