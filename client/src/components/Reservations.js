5149c8cbc560948db979dea440fd1eb0e5d47819
Samir Baraily <samir.baraily@gmail.com>
2019-01-16 20:56:16 -0700

    Pushing The Reservation Form

--- client/src/components/Reservations.js
+++ client/src/components/Reservations.js
@@ -2,7 +2,11 @@ import React from 'react'
 import axios from 'axios'
 import ReservationForm from './ReservationForm';
 import {Link} from 'react-router-dom';
+<<<<<<< HEAD
 import { Card, Container, Header, Table, Button, Segment } from 'semantic-ui-react';
+=======
+import { Card, Container, Header, Table, Segment } from 'semantic-ui-react';
+>>>>>>> Pushing The Reservation Form
 
 
 class Reservations extends React.Component {
@@ -15,21 +19,31 @@ class Reservations extends React.Component {
         })
     }
 
+<<<<<<< HEAD
     removeReserv = (id) => {
         axios.delete(`/api/reservations'/${id}`)
         const delReserv = this.state.reservations.filter( del => del.id !== id)
         this.setState({ reservations: [...delReserv]})
     }
 
+=======
+>>>>>>> Pushing The Reservation Form
     showReservations = () => {
         return this.state.reservations.map(d => {
             return (
         //         {/* <Link to={`/reservations/${d.id}`}>{d.name}
+<<<<<<< HEAD
     
           <Table singleLine>
               <Table.Header>
                   <Table.Row >
                       <Table.HeaderCell>No. </Table.HeaderCell>
+=======
+
+          <Table singleLine>
+              <Table.Header>
+                  <Table.Row>
+>>>>>>> Pushing The Reservation Form
                       <Table.HeaderCell>Name </Table.HeaderCell>
                       <Table.HeaderCell>Phone</Table.HeaderCell>
                       <Table.HeaderCell>Email</Table.HeaderCell>
@@ -39,19 +53,25 @@ class Reservations extends React.Component {
                   </Table.Row>
               </Table.Header>
               <Table.Body>
+<<<<<<< HEAD
                   <Table.Cell>{d.id}</Table.Cell>
+=======
+>>>>>>> Pushing The Reservation Form
                   <Table.Cell>{d.name}</Table.Cell>
                   <Table.Cell>{d.phone}</Table.Cell>
                   <Table.Cell>{d.email}</Table.Cell>
                   <Table.Cell>{d.date}</Table.Cell>
                   <Table.Cell>{d.time}</Table.Cell>
                   <Table.Cell>{d.party}</Table.Cell>
+<<<<<<< HEAD
                   <Table.Cell>
                       <Button icon color="red" onClick={() => this.removeReserv(d.id)}>Delete</Button>
                   </Table.Cell>
                   {/* <Table.Cell>
                       <Button icon color="green" onClick={() => this.edit()}>Edit</Button>
                   </Table.Cell> */}
+=======
+>>>>>>> Pushing The Reservation Form
               </Table.Body>
           </Table>
             )
@@ -68,9 +88,13 @@ class Reservations extends React.Component {
     render() {
         return (
             <Container>
+<<<<<<< HEAD
                 <br/><Header as="h1">Reservation List</Header><br/>
                 {this.showReservations()}
                 <br/><br/>
+=======
+                {this.showReservations()}
+>>>>>>> Pushing The Reservation Form
             </Container>
             
         );
