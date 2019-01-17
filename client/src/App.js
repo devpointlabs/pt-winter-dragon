4897fabf33df9cf5148a36efae6229af043db9a6
Samir Baraily <samir.baraily@gmail.com>
2019-01-16 20:56:16 -0700

    Pushing The Reservation Form

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Reservations from './components/Reservations';
-import Reservation from './components/Reservation';
+import ReservationForm from  './components/ReservationForm';
 // import Menu from './components/Menu';
 // import Cart from '/components/Cart';
 import Contact from './components/Contact';
@@ -18,7 +18,7 @@ const App = () => (
       {/* <Route exact path='/menu' component={Menu} /> */}
       {/* <Route exact path='/cart' component={Cart} /> */}
       <Route exact path='/reservations' component={Reservations} />
-      {/* <Route exact path='/reservations/:id' component={Reservation} /> */}
+      <Route exact path='/reservationform' component={ReservationForm} />
       <Route exact path='/contacts' component={Contact} />
       {/* <Route component={NoMatch} /> */}
 
