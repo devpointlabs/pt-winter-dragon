b360f23c2ec151a5a0ce2a8b65dfff907e2ab7ca
Mitchell Glazier <glazierm@gmail.com>
2019-01-17 18:57:55 -0700

    testing something

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
