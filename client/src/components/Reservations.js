b003e8d3f663e6966afdd98b94d935f1d03b23a7
Nikki Johnson <nikkijohnson@Nikkis-MacBook-Air.local>
2019-01-22 22:34:23 -0700

    should be fixed code

--- client/src/components/Reservations.js
+++ client/src/components/Reservations.js
@@ -20,7 +20,25 @@ class Reservations extends React.Component {
             return (
         //         {/* <Link to={`/reservations/${d.id}`}>{d.name}
 
-          <Table singleLine>
+          
+              
+              <Table.Body>
+                  <Table.Cell>{d.name}</Table.Cell>
+                  <Table.Cell>{d.phone}</Table.Cell>
+                  <Table.Cell>{d.email}</Table.Cell>
+                  <Table.Cell>{d.date}</Table.Cell>
+                  <Table.Cell>{d.time}</Table.Cell>
+                  <Table.Cell>{d.party}</Table.Cell>
+              </Table.Body>
+          
+            )
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <Table singleLine>
               <Table.Header>
                   <Table.Row>
                       <Table.HeaderCell>Name </Table.HeaderCell>
@@ -30,24 +48,9 @@ class Reservations extends React.Component {
                       <Table.HeaderCell>Requested Time</Table.HeaderCell>
                       <Table.HeaderCell>Number of People</Table.HeaderCell>
                   </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                  <Table.Cell>{d.name}</Table.Cell>
-                  <Table.Cell>{d.phone}</Table.Cell>
-                  <Table.Cell>{d.email}</Table.Cell>
-                  <Table.Cell>{d.date}</Table.Cell>
-                  <Table.Cell>{d.time}</Table.Cell>
-                  <Table.Cell>{d.party}</Table.Cell>
-              </Table.Body>
-          </Table>
-            )
-        })
-    }
-
-    render() {
-        return (
-            <Container>
+                  </Table.Header>
                 {this.showReservations()}
+                </Table>
             </Container>
         );
     }
