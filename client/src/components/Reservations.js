4891ee455d5644c3782d957ed809366005975e42
Nikki Johnson <nikkijohnson@Nikkis-MacBook-Air.local>
2019-01-26 07:38:26 -0700

    trying to add cloudinary

--- client/src/components/Reservations.js
+++ client/src/components/Reservations.js
@@ -28,17 +28,15 @@ class Reservations extends React.Component {
         return this.state.reservations.map(d => {
             return (
         //         {/* <Link to={`/reservations/${d.id}`}>{d.name}
-
-          
-              
-
-              <Table.Body>
+              <Table.Body key={d.id}>
+              <Table.Row>
                   <Table.Cell>{d.name}</Table.Cell>
                   <Table.Cell>{d.phone}</Table.Cell>
                   <Table.Cell>{d.email}</Table.Cell>
                   <Table.Cell>{d.date}</Table.Cell>
                   <Table.Cell>{d.time}</Table.Cell>
                   <Table.Cell>{d.party}</Table.Cell>
+              </Table.Row>
               </Table.Body>
 
               <Table.Cell>
@@ -58,17 +56,17 @@ class Reservations extends React.Component {
         return (
             <Container>
                 <Table singleLine>
-              <Table.Header>
-                  <Table.Row>
-                      <Table.HeaderCell>Name </Table.HeaderCell>
-                      <Table.HeaderCell>Phone</Table.HeaderCell>
-                      <Table.HeaderCell>Email</Table.HeaderCell>
-                      <Table.HeaderCell>Requested Date</Table.HeaderCell>
-                      <Table.HeaderCell>Requested Time</Table.HeaderCell>
-                      <Table.HeaderCell>Number of People</Table.HeaderCell>
-                  </Table.Row>
-                  </Table.Header>
-                {this.showReservations()}
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Name </Table.HeaderCell>
+                            <Table.HeaderCell>Phone</Table.HeaderCell>
+                            <Table.HeaderCell>Email</Table.HeaderCell>
+                            <Table.HeaderCell>Requested Date</Table.HeaderCell>
+                            <Table.HeaderCell>Requested Time</Table.HeaderCell>
+                            <Table.HeaderCell>Number of People</Table.HeaderCell>
+                        </Table.Row>
+                        </Table.Header>
+                        {this.showReservations()}
                 </Table>
 
             </Container>
