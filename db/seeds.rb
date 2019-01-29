380fc823e78f7f35124083c193a327f26fd80299
Nikki Johnson <nikkijohnson@Nikkis-MacBook-Air.local>
2019-01-28 20:16:13 -0700

    authentication and others

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,26 @@
-1.times do
-  menu = Menu.create(name: Faker::Commerce.department)
-  2.times do
-    category = Category.create(
-        name: Faker::Food.ingredient,
-        description: Faker::Food.description,
-    )
-    5.times do
-      item = Item.create(
-        name: Faker::Food.dish,
-        price: Faker::Commerce.material,
-        spice: Faker::Boolean.boolean,
-        # image: Faker::Boolean.boolean,
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+3.times do
+    menu = Menu.create(name: Faker::Commerce.department)
+    3.times do
+      category = Category.create(
+          menu_id: menu.id,
+          name: Faker::Food.ingredient,
+          description: Faker::Food.description
       )
+      5.times do
+        item = Item.create(
+          category_id: category.id, 
+          name: Faker::Food.dish,
+          price: rand(1.5-6.5),
+          spice: Faker::Boolean.boolean
+        )
+      end
     end
   end
 end
