2669d6f5e2a206d3d588156c430facf5ceae3a7a
Samir Baraily <samir.baraily@gmail.com>
2019-01-28 20:50:51 -0700

    more commits

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_01_24_204918) do
-=======
 ActiveRecord::Schema.define(version: 2019_01_27_171949) do
->>>>>>> f2f806631f001e14ec736e4868f594318743fffd
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,13 +90,8 @@ ActiveRecord::Schema.define(version: 2019_01_27_171949) do
   end
 
   create_table "taxnfees", force: :cascade do |t|
-<<<<<<< HEAD
-    t.float "tax"
-    t.float "delivery"
-=======
     t.float "delivery"
     t.float "tax"
->>>>>>> f2f806631f001e14ec736e4868f594318743fffd
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
